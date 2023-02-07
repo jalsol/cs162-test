3562642f9669e1322cb49f2b694dd01e8700a47f
jalsol <jalsol@pm.me>
2023-02-07 19:26:35 +0700

    Add project configurations

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout
+        <<
+        "hi\n"
+        ;
+
+    // constexpr int num=100;
+    for(int i=0;i<100;++i){
+        std::cout<<i<<std::endl;
+    }
+}
